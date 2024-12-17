ebf70d76555dd5c32fdc5d76e0c31bd6065dfcfe
Sanjit Mohan Rane <sanjit@Sanjits-MacBook-Pro.local>
2024-12-17 20:43:15 +0530

    Added plugin to copy assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -48,5 +49,8 @@ module.exports = {
       template: './public/index.html',
     }),
     new Dotenv(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'public/assets', to: 'assets' }],
+    }),
   ],
 };
